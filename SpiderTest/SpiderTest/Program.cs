e0921355cf609c308d4fc8ca3c1c5225ff7d44c5
hq <hq@ALTMANXIANG>
2016-09-03 15:44:57 +0800

    新增npoi操作excel练习

--- SpiderTest/SpiderTest/Program.cs
+++ SpiderTest/SpiderTest/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PracticePlot;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,19 +15,21 @@ namespace SpiderTest
     {
         static void Main(string[] args)
         {
-            PicSpider logic = new PicSpider();
-            string result = logic.SpiderDo("http://huaban.com/search/?q=car&intxsxgw&page=2&per_page=20&wfl=1", "", "");
-            Regex regex = new Regex("csrf-token\" content=\"(?<token>[\\s\\S]*?)\" />", RegexOptions.Compiled);
-            if (regex.IsMatch(result))
-            {
-                result = regex.Match(result).Groups["token"].Value;
-            }
-            result = logic.SpiderDo("https://api.500px.com/v1/photos?rpp=50&feature=popular&image_size%5B%5D=1&image_size%5B%5D=2&image_size%5B%5D=32&image_size%5B%5D=31&image_size%5B%5D=33&image_size%5B%5D=34&image_size%5B%5D=35&image_size%5B%5D=36&image_size%5B%5D=2048&image_size%5B%5D=4&sort=&include_states=true&formats=jpeg%2Clytro&only=&page=1&rpp=50", "https://500px.com/popular", result);
-            logic.ConvertJson(result);
+            //PicSpider logic = new PicSpider();
+            //string result = logic.SpiderDo("http://huaban.com/search/?q=car&intxsxgw&page=2&per_page=20&wfl=1", "", "");
+            //Regex regex = new Regex("csrf-token\" content=\"(?<token>[\\s\\S]*?)\" />", RegexOptions.Compiled);
+            //if (regex.IsMatch(result))
+            //{
+            //    result = regex.Match(result).Groups["token"].Value;
+            //}
+            //result = logic.SpiderDo("https://api.500px.com/v1/photos?rpp=50&feature=popular&image_size%5B%5D=1&image_size%5B%5D=2&image_size%5B%5D=32&image_size%5B%5D=31&image_size%5B%5D=33&image_size%5B%5D=34&image_size%5B%5D=35&image_size%5B%5D=36&image_size%5B%5D=2048&image_size%5B%5D=4&sort=&include_states=true&formats=jpeg%2Clytro&only=&page=1&rpp=50", "https://500px.com/popular", result);
+            //logic.ConvertJson(result);
 
-            logic.DelImage();
+            //logic.DelImage();
 
-            Console.ReadLine();
+            //Console.ReadLine();
+            NPOIPractice NPOIpractice = new NPOIPractice();
+            NPOIpractice.ExcelToDateTable();
         }
     }
 }
